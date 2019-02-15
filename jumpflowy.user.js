d604df69317f718216086efb92840b6e321a1cb5
Matt Hutton <mbhutton@gmail.com>
2019-02-16 08:59:05 +1300

    Bump version

--- jumpflowy.user.js
+++ jumpflowy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JumpFlowy
 // @namespace    https://github.com/mbhutton/jumpflowy
-// @version      0.1.6.6
+// @version      0.1.6.7
 // @description  WorkFlowy user script for search and navigation
 // @author       Matt Hutton
 // @match        https://workflowy.com/*
