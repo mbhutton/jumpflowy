00c20eec8d0f5a8176d53504f68178dae18c6310
Matt Hutton <mbhutton@gmail.com>
2020-03-19 06:33:11 +1300

    Bump version to 0.1.8.19

--- jumpflowy.user.js
+++ jumpflowy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JumpFlowy
 // @namespace    https://github.com/mbhutton/jumpflowy
-// @version      0.1.8.18
+// @version      0.1.8.19
 // @description  WorkFlowy user script for search and navigation
 // @author       Matt Hutton
 // @match        https://workflowy.com/*
